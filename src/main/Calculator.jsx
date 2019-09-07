69bdc3fe7c8b387b88cb1c1ae16064e9cddc0562
Carla Sampaio <carlamsampaio01@gmail.com>
2019-09-07 11:15:16 +0100

    finishing button component

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,29 +2,45 @@ import React, { Component } from 'react';
 import './Calculator.css'
 import Button from '../components/Button';
 import Display from '../components/Display';
+import { clear } from 'sisteransi';
 
 export default class Calculator extends Component {
+    
+    clearMemory(){
+        console.log('limpar')
+    }
+    setOperation(operation){
+        console.log(operation)
+    }
+    addDigit(digit){
+        console.log(digit)
+    }
+    
     render() {
+        const addDigit = n => this.addDigit(n)
+        const clearMemory = this.clearMemory()
+        const setOperation = op => this.setOperation(op)
+       
         return (
             <div className="calculator">
-                <Display value="100"></Display>
-                <Button label="AC"></Button>
-                <Button label="/"></Button>
-                <Button label="7"></Button>
-                <Button label="8"></Button>
-                <Button label="9"></Button>
-                <Button label="*"></Button>
-                <Button label="4"></Button>
-                <Button label="5"></Button>
-                <Button label="6"></Button>
-                <Button label="-"></Button>
-                <Button label="1"></Button>
-                <Button label="2"></Button>
-                <Button label="3"></Button>
-                <Button label="+"></Button>
-                <Button label="0"></Button>
-                <Button label="."></Button>
-                <Button label="="></Button>
+                <Display value="100" ></Display>
+                <Button label="AC" onClick={() => clearMemory()} triple></Button>
+                <Button label="/" onClick={() => setOperation} operation></Button>
+                <Button label="7" onClick={() => addDigit}></Button>
+                <Button label="8" onClick={() => addDigit}></Button>
+                <Button label="9" onClick={() => addDigit}></Button>
+                <Button label="*" onClick={() => setOperation} operation></Button>
+                <Button label="4" onClick={() => addDigit}></Button>
+                <Button label="5" onClick={() => addDigit}></Button>
+                <Button label="6" onClick={() => addDigit}></Button>
+                <Button label="-" onClick={() => setOperation} operation></Button>
+                <Button label="1" onClick={() => addDigit}></Button>
+                <Button label="2" onClick={() => addDigit}></Button>
+                <Button label="3" onClick={() => addDigit}></Button>
+                <Button label="+" onClick={() => setOperation} operation></Button>
+                <Button label="0" onClick={() => addDigit} double></Button>
+                <Button label="." onClick={() => addDigit}></Button>
+                <Button label="=" onClick={() => setOperation} operation></Button>
             </div>
         )
     }
